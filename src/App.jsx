6ee03979e8c569f56eff9e867bb1267c5cfc4db7
Havoac <shivanshsingh9670@gmail.com>
2025-06-01 20:32:31 +0530

    Base Structure prepared

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Home from "./pages/Home/Home";
+
+function App() {
+  return <>
+  <div className="App">
+    <Router>
+        <Header></Header>
+      <Routes>
+        <Route index element={ <Home></Home> } ></Route>
+        <Route index element={<h1>Hello</h1>}></Route>
+        <Route path="/movie/:id" element={<h1>Movie Id</h1>}></Route>
+        <Route path="/movies/:type" element={<h1>Movie Type</h1>}></Route>
+        <Route path="/*" element={<h1>Error Page</h1>}></Route>
+      </Routes>
+    </Router>
+  </div>
+  </>;
+}
+
+export default App;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Home.css'
+
+const home = () => {
+    return <></>
+}
+
+export default home;
