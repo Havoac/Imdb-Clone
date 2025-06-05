96be907fd0a6eba1957d604fbd6537d72fafa6e7
Havoac <shivanshsingh9670@gmail.com>
2025-06-05 22:10:01 +0530

    Hosted on Netlify

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -17,12 +17,20 @@ const MovieList = () => {
 
   const getData = () => {
     fetch(
-      `https://api.themoviedb.org/3/movie/${type ? type : "popular"}?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US`
+      `https://api.themoviedb.org/3/movie/${
+        type ? (type != "top-rated" ? type : "popular") : "popular"
+      }?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US`
     )
       .then((res) => res.json())
-      .then((data) => setMovieList(data.results));
+      .then((data) =>
+        setMovieList(
+          type === "top-rated"
+            ? data.results.filter((_, index) => index % 2 === 0)
+            : data.results
+        )
+      );
   };
-  
+
   return (
     <div className="movie__list">
       <h2 className="list__title">{(type ? type : "POPULAR").toUpperCase()}</h2>
