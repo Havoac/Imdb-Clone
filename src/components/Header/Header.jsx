9883b64dfd625c9c162458c16321b97504070952
Havoac <shivanshsingh9670@gmail.com>
2025-06-28 20:54:30 +0530

    Minor Changes for responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Header.css";
+import headerImg from "../../assets/moviescope.png";
 
 const Header = () => {
   return (
@@ -10,18 +11,25 @@ const Header = () => {
           <Link to="/">
             <img
               className="header__icon"
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/2560px-IMDB_Logo_2016.svg.png"
+              src={headerImg}
+              style={{
+                transform: "scale(1.7)",
+                maxWidth: "100%",
+                height: "auto"
+              }}
             />
           </Link>
-          <Link to="/movies/popular" style={{ textDecoration: "none" }}>
-            <span>Popular</span>
-          </Link>
-          <Link to="/movies/top-rated" style={{ textDecoration: "none" }}>
-            <span>Top Rated</span>
-          </Link>
-          <Link to="/movies/upcoming" style={{ textDecoration: "none" }}>
-            <span>Upcoming</span>
-          </Link>
+          <div className="navLinks">
+            <Link to="/movies/popular" style={{ textDecoration: "none" }}>
+              <span>Popular</span>
+            </Link>
+            <Link to="/movies/top-rated" style={{ textDecoration: "none" }}>
+              <span>Top Rated</span>
+            </Link>
+            <Link to="/movies/upcoming" style={{ textDecoration: "none" }}>
+              <span>Upcoming</span>
+            </Link>
+          </div>
         </div>
       </div>
     </>
